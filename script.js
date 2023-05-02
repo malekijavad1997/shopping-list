7f4c3a1a1a96461f9ac59578a3be757c27144ae5
malekijavad1997 <131247748+malekijavad1997@users.noreply.github.com>
2023-05-02 21:11:18 +0430

    adding New Files

---
+++ script.js
@@ -0,0 +1,29 @@
+var todo=[
+"clean room",
+"brush teeth",
+"exercise",
+"Study js",
+"eat food"
+];
+
+
+todo.forEach(function (item,i) {
+	console.log(item,i);
+})
+
+
+// var todolength=todo.length;
+// for (var i = 0; i <todolength; i++) {
+// 	// todo[i]=todo[i]+"!";
+// 	todo.pop();
+// 	// console.log(todo[i]);
+// }
+
+
+
+// var counterOne=0;
+// while(counterOne<10){
+// 	console.log(counterOne);
+// 	counterOne++;
+
+// }
